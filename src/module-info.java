ea52296706594e0376bf1e0df3a27a363d272d16
Malak1993 <127646362+Malak1993@users.noreply.github.com>
2023-05-01 12:07:12 -0500

    committed

---
+++ src/module-info.java
@@ -0,0 +1,11 @@
+/**
+ * 
+ */
+/**
+ * @author Abdel Ali
+ *
+ */
+module selenium {
+	requires org.seleniumhq.selenium.api;
+	requires org.seleniumhq.selenium.chrome_driver;
+}
---
+++ src/variousConsept/FirstClassSelenium.java
@@ -0,0 +1,10 @@
+package variousConsept;
+
+public class FirstClassSelenium {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
